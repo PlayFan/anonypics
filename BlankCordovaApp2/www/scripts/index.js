5bb1c75f4e71a17b429a608a90817ab78bde0241
Florent Michenaud <florentm@MacBook-Pro-de-Florent.local>
2017-05-03 11:32:27 -0400

    first commit

---
+++ BlankCordovaApp2/www/scripts/index.js
@@ -0,0 +1,86 @@
+﻿// Pour obtenir une présentation du modèle Vide, consultez la documentation suivante :
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// Pour déboguer du code durant le chargement d'une page dans cordova-simulate ou sur les appareils/émulateurs Android, lancez votre application, définissez des points d'arrêt, 
+// puis exécutez "window.location.reload()" dans la console JavaScript.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    $(document).on('deviceready', function deviceIsReady() {
+   console.log('Device is ready!');
+   setupPush();
+ });
+    function onDeviceReady() {
+      $.getJSON('http://playfanfip.cluster021.hosting.ovh.net/upload/imageDb.json', function (data) {
+                        $.each(data, function (i, field) {
+                            if (i < 25) {
+                                $("#divImage").append("<img class='popphoto' src='" + field + "'></img>");
+                            }
+                        });
+                    });
+        setupPush();
+        // Gérer les événements de suspension et de reprise Cordova
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        // TODO: Cordova a été chargé. Effectuez l'initialisation qui nécessite Cordova ici.
+        var parentElement = document.getElementById('deviceready');
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    };
+
+    function onPause() {
+        // TODO: cette application a été suspendue. Enregistrez l'état de l'application ici.
+    };
+
+    function onResume() {
+        // TODO: cette application a été réactivée. Restaurez l'état de l'application ici.
+    };
+    function setupPush() {
+   var push = PushNotification.init({
+       "android": {
+           "senderID": "14715393201"
+       },
+       "ios": {
+         "sound": true,
+         "alert": true,
+         "badge": true
+       },
+       "windows": {}
+   });
+
+   push.on('registration', function(data) {
+       console.log("registration event: " + data.registrationId);
+       var oldRegId = localStorage.getItem('registrationId');
+       if (oldRegId !== data.registrationId) {
+           // Save new registration ID
+           localStorage.setItem('registrationId', data.registrationId);
+           // Post registrationId to your app server as the value has changed
+       }
+   });
+
+   push.on('error', function(e) {
+       console.log("push error = " + e.message);
+   });
+        push.on('notification', function(data) {
+         console.log('notification event');
+         navigator.notification.alert(
+             data.message,         // message
+             null,                 // callback
+             data.title,           // title
+             'Ok'                  // buttonName
+         );
+
+         push.finish(function() {
+             console.log('success');
+         }, function() {
+             console.log('error');
+         });
+     });
+
+ }
+    
+
+} )();
